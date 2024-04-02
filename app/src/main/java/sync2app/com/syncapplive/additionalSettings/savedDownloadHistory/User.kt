59cc9a7cb04d24eb53078e6e75d78342d7e08a27
HP <tigera1759@gmail.com>
2024-04-02 12:29:44 +0100

    April 2 - Modified PT Time

--- app/src/main/java/sync2app/com/syncapplive/additionalSettings/savedDownloadHistory/User.kt
+++ app/src/main/java/sync2app/com/syncapplive/additionalSettings/savedDownloadHistory/User.kt
@@ -3,7 +3,7 @@ package sync2app.com.syncapplive.additionalSettings.savedDownloadHistory
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "user_table", primaryKeys = ["CLO", "DEMO", "EditUrl"])
+@Entity(tableName = "user_table", primaryKeys = ["CLO", "DEMO", "EditUrl", "EditUrlIndex"])
 data class User(
     var id: Long = System.currentTimeMillis(),
     var CLO: String,
